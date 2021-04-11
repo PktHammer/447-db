6454178eeca1af215c8e687f99daa295a5820a30
Cris Trinidad <ctrini1@umbc.edu>
2021-04-11 16:22:29 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -67,16 +67,16 @@ dtype_prison_data = {
 def update():
     main_covid_data = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
     main_prison_data = "https://raw.githubusercontent.com/uclalawcovid19behindbars/historical-data/main/data/CA-historical-data.csv"
-    df_covid = pd.read_csv(main_covid_data, dtype = dtype_covid_data)
-    df_prison = pd.read_csv(main_prison_data, dtype = dtype_prison_data)
+    df_covid = pd.read_csv(main_covid_data)
+    df_prison = pd.read_csv(main_prison_data)
 
     covid_table_name = "main_covid_data"
     prison_table_name = "main_prison_data"
     engine_string = 'mysql+pymysql://' + secrets_ignore.user + ":" + secrets_ignore.password + "@" + secrets_ignore.ip_endpoint + "/" + secrets_ignore.db_name
     engine = create_engine(engine_string)
     dbConnection = engine.connect()
-    send_frame_covid = df_covid.to_sql(covid_table_name, dbConnection, if_exists='replace')
-    send_frame_prison = df_prison.to_sql(prison_table_name, dbConnection, if_exists='replace')
+    send_frame_covid = df_covid.to_sql(covid_table_name, dbConnection, if_exists='replace', dtype=dtype_covid_data)
+    send_frame_prison = df_prison.to_sql(prison_table_name, dbConnection, if_exists='replace', dtype=dtype_prison_data)
 
     # Close conn
     dbConnection.close()
