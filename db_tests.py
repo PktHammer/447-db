8fc4ae5dda02754ef32c0e64aacbfae14757f25e
Josh Liao <44016363+PktHammer@users.noreply.github.com>
2021-05-16 20:54:16 -0400

    Housekeeping

--- db_tests.py
+++ db_tests.py
@@ -10,6 +10,11 @@ import random
 import string
 import db_return_codes
 
+#######################
+# Arbitrary settings that may need to be changed in the future
+# Test URL & Table Name for UAT test
+test_data_csv_url = "https://data.chhs.ca.gov/dataset/e283ee5a-cf18-4f20-a92c-ee94a2866ccd/resource/130d7ba2-b6eb-438d-a412-741bde207e1c/download/covid19vaccinesbycounty.csv"
+test_tbl_name = "oaisjdfoajsodifjoaiwaskdjfei"
 
 #######################
 # Test Helper Functions
@@ -148,8 +153,7 @@ def db_main_query_tests() -> list:
 def db_UAT_tests():
     failed_tests = []
     # Test csv
-    test_data_csv_url = "https://data.chhs.ca.gov/dataset/e283ee5a-cf18-4f20-a92c-ee94a2866ccd/resource/130d7ba2-b6eb-438d-a412-741bde207e1c/download/covid19vaccinesbycounty.csv"
-    test_tbl_name = "oaisjdfoajsodifjoaiwaskdjfei"
+
 
     test_username, test_password = generate_user()
 
